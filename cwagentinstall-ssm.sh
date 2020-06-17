db63b229a7d0fd876a36190a078ab5d8b6c9c021
DaveGCode <dgregan10@gmail.com>
2020-06-17 16:35:32 +0000

    refactor

--- cwagentinstall-ssm.sh
+++ cwagentinstall-ssm.sh
@@ -3,6 +3,5 @@ mkdir /tmp/AmazonCloudWatchAgent
 cd /tmp/AmazonCloudWatchAgent
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/linux/amd64/latest/AmazonCloudWatchAgent.zip
 unzip AmazonCloudWatchAgent.zip
-sudo ./install.sh
-touch /opt/aws/amazon-cloudwatch-agent/bin/config.json
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux-config.json -s
+./install.sh
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c ssm:AmazonCloudWatch-linux-config.json -s
